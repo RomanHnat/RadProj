2798eb6ed01fb202f9e8917717dd13addf9fae2c
Roman <roman.hnatyshyn@students.ittralee.ie>
2022-11-16 18:45:27 +0000

    Signed-off-by: Roman <roman.hnatyshyn@students.ittralee.ie>

--- RAD Unity/Assets/CharacterControl.cs
+++ RAD Unity/Assets/CharacterControl.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     float speed = 7f;
+    float sprint = 5f;
+    public bool ground;
+    public float jump = 7f;
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -40,12 +45,24 @@ public class CharacterControl : MonoBehaviour
 
         }
 
-       
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
 
-       
-        
-
-       
+            transform.position += sprint * transform.forward * Time.deltaTime;
         }
+     
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 300, 0));
+        }
+
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+}
